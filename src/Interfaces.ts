84b99e86603a974c154aaa8e6be08761cb870221
B0rjomi <valerkalifanov@gmail.com>
2022-11-19 10:56:04 +0300

    solved interfaces task

--- src/Interfaces.ts
+++ src/Interfaces.ts
@@ -11,7 +11,7 @@ export function getPerson(): IPersonData {
     return {
         name: 'Petr',
         secondName: 'Smith',
-        age: '7',
+        age: 7,
     };
 }
 
@@ -27,6 +27,29 @@ export interface IStudentData {
 Данные должны храниться в объектах, соответствующих интерфейсу `IStudentData`.
  */
 export function getStudentsData(): IStudentData[] {
+    let arr: IStudentData[] = [];
+    let stud1, stud2, stud3: IStudentData;
+    stud1 = {
+        name: 'Ivan',
+        secondName: 'Petrov',
+        age: 20,
+        phone: '+7(555)555-55-50',
+    };
+    stud2 = {
+        name: 'Stepan',
+        secondName: 'Petrov',
+        age: 19,
+        phone: '+7(555)555-55-51',
+    };
+    stud3 = {
+        name: 'Petr',
+        secondName: 'Ivanov',
+        age: 19,
+    };
+    arr.push(stud1);
+    arr.push(stud2);
+    arr.push(stud3);
+    return arr;
     /**
      * Данные студентов которые нужно передать:
      *
